33d4d0e24e4db6a4e7f978e0faf2c63745a5b22a
S-Riches <samuelrichesprivate@gmail.com>
2022-10-12 00:45:59 +0100

    accidentally forgot to add the deletions

--- BackEnd/blogBackend/blogAPI/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . import models
-from models import file
-
-# Register your models here.
--- BackEnd/blogBackend/blogAPI/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-# define a model by creating a class with values that line up to a sql table
-class file(models.Model):
-    # name of the document
-    documentTitle = models.CharField(max_length=50)
-    # document data is stored as a bytea
-    data = models.BinaryField(null=True)

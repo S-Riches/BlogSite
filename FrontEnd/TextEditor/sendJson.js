2ce445cbac6677d084433e86d8a4ed55de377546
S-Riches <samuelrichesprivate@gmail.com>
2022-10-14 18:21:26 +0100

    worked on portfolio today, but github doesnt track other branches in the contribution tracker so here we are with tiny changes on my main project

--- FrontEnd/TextEditor/sendJson.js
+++ FrontEnd/TextEditor/sendJson.js
@@ -1,24 +1,25 @@
-// convert the data within the elements into a blob, then send to the api via a post request
+// convert the data within the elements into a FormData, then send to the api via a post request
 async function sendData() {
     let markDownContent = document.getElementById("textEditor").value;
     let fileName = document.getElementById("textEditorTitle").value;
-    // convert data to json
-    let data = new Blob([JSON.stringify(fileName, markDownContent)], {
-        type: "application/json",
-    });
+    // convert data to FormData Obj
+    let data = new FormData();
+    data.append("Title", fileName);
+    data.append("Content", markDownContent);
     // POST request to the django backend
     const send = await fetch("http://127.0.0.1:8000/createFile/markDownSend/", {
         method: "POST",
-        mode: "no-cors",
+        mode: "cors",
         credentials: "same-origin",
         headers: {
-            "Content-Type": "text/html",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://127.0.0.1:8000",
         },
-        body: data,
-        keepalive: true,
-    })
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+        keepalive: "true",
+        body: JSON.stringify(data),
+    });
+    const responseJson = await send.json();
+    console.log(responseJson);
 }
 
 document.getElementById("sendData").addEventListener("click", () => {

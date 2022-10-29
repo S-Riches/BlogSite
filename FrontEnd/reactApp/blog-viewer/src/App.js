a7faeccd14a68ccd00370f767813cfdb8b158f1f
samriches <Samuelrichesprivate@gmail.com>
2022-10-29 22:25:52 +0100

    didnt do enough work today after work - too tired but looked into react props

--- FrontEnd/reactApp/blog-viewer/src/App.js
+++ FrontEnd/reactApp/blog-viewer/src/App.js
@@ -3,12 +3,22 @@ import Navbar from "./Navbar";
 import Card from "./Card";
 
 function App() {
+    // test data for the props
+    const blogs = ["Foo", "bar", "yee", "har"];
     return (
         <div className="App">
             <Navbar />
             <div className="cardHolder">
-                <Card />
-                <Card />
+                {/* can pass down values into components via props. useful for reusing code. */}
+                <Card
+                    article={blogs[Math.floor(Math.random() * blogs.length)]}
+                />
+                <Card
+                    article={blogs[Math.floor(Math.random() * blogs.length)]}
+                />
+                <Card
+                    article={blogs[Math.floor(Math.random() * blogs.length)]}
+                />
             </div>
         </div>
     );
--- FrontEnd/reactApp/blog-viewer/src/Card.js
+++ FrontEnd/reactApp/blog-viewer/src/Card.js
@@ -1,11 +1,12 @@
-const Card = () => {
+const Card = (props) => {
+    const article = props.article;
     // want to pass in text from the database.
     return (
         <div className="card">
             <div className="cardBox">
                 <h3>Title</h3>
                 {/* gonna need to figure out how to populate the card through the params. */}
-                <p>Content</p>
+                <p>{article}</p>
             </div>
         </div>
     );
